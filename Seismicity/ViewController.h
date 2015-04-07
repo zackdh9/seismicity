8d7fedbaca6b91e129bc7847cc8580c0671899b5
zack <zack@zacks-Mac.local>
2015-04-07 00:39:09 -0500

    Initial Commit

---
+++ Seismicity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Seismicity
+//
+//  Created by zack on 4/7/15.
+//  Copyright (c) 2015 Zachary Hamblen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Seismicity/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Seismicity
+//
+//  Created by zack on 4/7/15.
+//  Copyright (c) 2015 Zachary Hamblen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
